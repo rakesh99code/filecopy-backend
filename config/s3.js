9caf814b77f849dea53762f59960ca5fdc0b054c
Rakesh Reddy <rakeshreddywebdev@gmail.com>
2026-06-07 14:33:17 +0530

    complete backend

---
+++ config/s3.js
@@ -0,0 +1,74 @@
+import {
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+  GetObjectCommand,
+  HeadObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+export const s3Client = new S3Client();
+
+export const createUploadSignedUrl = async ({ key, contentType }) => {
+  const command = new PutObjectCommand({
+    Bucket: "filecopy-storageapp",
+    Key: key,
+    ContentType: contentType,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+    signableHeaders: new Set(["content-type"]),
+  });
+
+  return url;
+};
+
+export const createGetSignedUrl = async ({
+  key,
+  download = false,
+  filename,
+}) => {
+  const command = new GetObjectCommand({
+    Bucket: "filecopy-storageapp",
+    Key: key,
+    ResponseContentDisposition: `${download ? "attachment" : "inline"}; filename=${encodeURIComponent(filename)}`,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+  });
+
+  return url;
+};
+
+export const getS3FileMetaData = async (key) => {
+  const command = new HeadObjectCommand({
+    Bucket: "filecopy-storageapp",
+    Key: key,
+  });
+
+  return await s3Client.send(command);
+};
+
+export const deleteS3File = async (key) => {
+  const command = new DeleteObjectCommand({
+    Bucket: "filecopy-storageapp",
+    Key: key,
+  });
+
+  return await s3Client.send(command);
+};
+
+export const deleteS3Files = async (keys) => {
+  const command = new DeleteObjectsCommand({
+    Bucket: "filecopy-storageapp",
+    Delete: {
+      Objects: keys,
+      Quiet: false, // set true to skip individual delete responses
+    },
+  });
+
+  return await s3Client.send(command);
+};
