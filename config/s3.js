c94bcd6b00d945fac8dbce254274d857b3032e74
Rakesh Reddy <rakeshreddywebdev@gmail.com>
2026-06-07 15:42:10 +0530

    update urls

--- config/s3.js
+++ config/s3.js
@@ -8,7 +8,12 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-export const s3Client = new S3Client();
+export const s3Client = new S3Client({
+  credentials:{
+    accessKeyId:'AKIATKI7KK2VH3V4DLE6',
+    secretAccessKey: process.env.AWS_SecretKey
+  }
+});
 
 export const createUploadSignedUrl = async ({ key, contentType }) => {
   const command = new PutObjectCommand({
